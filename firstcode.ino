ab705b71d802cbae8354921274ee9228b6da716d
daniakiki <124864995+daniakiki@users.noreply.github.com>
2023-04-17 22:41:05 +0700

    penambahan program button
    penambahan program button

--- firstcode.ino
+++ firstcode.ino
@@ -1,8 +1,19 @@
+const int mybutton = 14;
+
 void setup(){
     Serial.begin(9600);
     ini di program f/button
+    pinMode(mybutton, INPUT);
 }
 
 void loop(){
+    int buttonState = digitalRead(mybutton);
     
+    if (buttonState == HIGH)
+    {
+        digitalWrite(myled, HIGH);
+    }
+    else{
+        digitalWrite(myled, LOW);
+    }
 }
